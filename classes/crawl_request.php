a390dd7e5230e6ff07853c88cd71fab6774b65c9
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-05-14 14:26:36 +0100

    Removed http header directives

--- classes/crawl_request.php
+++ classes/crawl_request.php
@@ -7,10 +7,6 @@
  * @since 0.9
  */
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-
 if($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
   die();
 }
