fc6dcf7393ee81308365794cc14f015d86aa8237
Ryan Jarrett <ryan.jarrett@digital.justice.gov.uk>
2015-11-24 12:23:03 +0000

    Use theme function for retrieving author info

--- classes/post_request.php
+++ classes/post_request.php
@@ -36,31 +36,7 @@ class post_request extends search_request {
 	      $alt_text = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 
 				if(function_exists('get_coauthors')) {
-					$authors_array = get_coauthors();
-					$authors = null;
-					foreach($authors_array as $author) {
-						$author_id = (int) $author->ID;
-						if($author->data) {
-							$author_name = $author->data->display_name;
-							$author_thumb = get_avatar_url($author_id);
-						} else {
-							$author_name = $author->display_name;
-							$author_thumb_id = get_post_thumbnail_id($author_id);
-							$author_thumb = wp_get_attachment_image_src($author_thumb_id, 'user-thumb')[0];
-						}
-						$authors[] = array(
-							// 'all_data' => $author,
-							'id'            => $author_id,
-							'name'          => $author_name,
-							'thumbnail_url' => $author_thumb
-						);
-					}
-				} else {
-					$authors = array(
-						'id'            => get_the_author_meta('ID',$post->ID),
-						'name'          => get_the_author_meta('display_name',$post->ID),
-						'thumbnail_url' => get_avatar_url(get_the_author_meta('ID',$post->ID))
-					);
+					$authors = dw_get_author_info($post->ID);
 				}
 
 	     	$this->results_array['results'][] = array(
